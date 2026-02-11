aaac19790ae484c76dbaa860c7dcfc72d8e74088
Allosauruscs <cg1186682@swccd.edu>
2026-02-11 23:10:18 +0000

    EuclideanDivision.java

--- EuclideanDivision.java
+++ EuclideanDivision.java
@@ -5,17 +5,23 @@ import java.util.Scanner;
 public class EuclideanDivision {
     public static void main(String[] args) {
         int dividend, divisor, quotient, remainder;
+        
         // Scanner object to receive keyboard input
         Scanner input = new Scanner(System.in);
         
         System.out.println("Euclidean Division");
+        
         // Prompt the user to enter an integer
         System.out.print("Enter the dividend: ");
         dividend = input.nextInt();
+
         System.out.print("Enter the divisor: ");
         divisor = input.nextInt();
+
         quotient = dividend / divisor;
         remainder = dividend % divisor;
+
+        // Display the number
         System.out.println(dividend + " = " + divisor + " * " + quotient + " + " + remainder);
     }
 }
